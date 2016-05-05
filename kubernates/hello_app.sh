c4d80b7c29c265aa87f48518d4617b018b0ef554
enixdark <cqshinn92@gmail.com>
2016-05-05 02:46:02 +0000

    add simple kubernate start with docker and simple .yaml file ( pod, service, rc)

---
+++ kubernates/hello_app.sh
@@ -0,0 +1,1 @@
+kubectl -s http://localhost:8080 run hello-world --image=tutum/hello-world --replicas=1 --port=80
---
+++ kubernates/service_proxy.sh
@@ -0,0 +1,1 @@
+sudo docker run -d --net=host --privileged gcr.io/google_containers/hyperkube:v1.0.1 /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
