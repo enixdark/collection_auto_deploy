2025eee5c55ca58cc6335b91cd6635eff1ee7f08
enixdark <cqshinn92@gmail.com>
2016-07-29 15:58:32 +0700

    update

--- kubernetes/kub1.3/kuber-master.sh
+++ kubernetes/kub1.3/kuber-master.sh
@@ -1,4 +1,5 @@
-export K8S_VERSION=$(curl -sS https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+# export K8S_VERSION=v1.3.0
+$(curl -sS https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 export ARCH=amd64
 # docker run -d \
 #     --volume=/:/rootfs:ro \
@@ -36,6 +37,27 @@ export ARCH=amd64
 #   gcr.io/google_containers/hyperkube:v1.0.1 \
 #   /hyperkube kubelet --containerized --hostname-override="127.0.0.1" --address="0.0.0.0" --api-servers=http://localhost:8080 --config=/etc/kubernetes/manifests
 
+# docker run -d \
+#     --volume=/:/rootfs:ro \
+#     --volume=/sys:/sys:rw \
+#     --volume=/var/lib/docker/:/var/lib/docker:rw \
+#     --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
+#     --volume=/var/run:/var/run:rw \
+#     --net=host \
+#     --pid=host \
+#     --privileged \
+#     gcr.io/google_containers/hyperkube-${ARCH}:${K8S_VERSION} \
+#     /hyperkube kubelet \
+#         --containerized \
+#         --hostname-override=127.0.0.1 \
+#         --address=0.0.0.0 \
+#         --api-servers=http://localhost:8080 \
+#         --config=/etc/kubernetes/manifests \
+#         --cluster-dns=10.0.0.10 \
+#         --cluster-domain=cluster.local \
+#         --allow-privileged --v=2
+
+export ARCH=amd64
 docker run -d \
     --volume=/:/rootfs:ro \
     --volume=/sys:/sys:rw \
@@ -49,9 +71,9 @@ docker run -d \
     /hyperkube kubelet \
         --containerized \
         --hostname-override=127.0.0.1 \
-        --address=0.0.0.0 \
         --api-servers=http://localhost:8080 \
         --config=/etc/kubernetes/manifests \
-        --cluster-dns=10.10.0.10 \
+        --cluster-dns=10.0.0.10 \
         --cluster-domain=cluster.local \
         --allow-privileged --v=2
+        
