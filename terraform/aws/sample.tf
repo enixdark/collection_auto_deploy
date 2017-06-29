bf56faa890c42b3c6678b00287e342090232e7fe
enixdark <cqshinn92@gmail.com>
2017-06-29 10:27:28 +0700

    update: add auto scale group

--- terraform/aws/sample.tf
+++ terraform/aws/sample.tf
@@ -4,7 +4,7 @@ variable "server_port" {
 }
 
 output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+  value = "${aws_launch_configuration.example.public_ip}"
 }
 
 provider "aws" {
@@ -13,19 +13,36 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  tags {
-    Name = "terraform-example"
-  }
+  security_groups = ["${aws_security_group.instance.id}"]
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+
+# resource "aws_instance" "example" {
+#   ami           = "ami-40d28157"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+#   tags {
+#     Name = "terraform-example"
+#   }
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World" > index.html
+#               nohup busybox httpd -f -p "${var.server_port}" &
+#               EOF
+# }
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -36,4 +53,24 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
